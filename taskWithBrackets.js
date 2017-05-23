57076af2a6faf0beee8c7ee37b4614336a427055
Aleksandr Odinok <Aleksandr.Odinok@AudienceScience.com>
2017-05-23 16:56:18 +0300

    Added straightforward solution for task with brackets

---
+++ taskWithBrackets.js
@@ -0,0 +1,24 @@
+const someStr = '({(sdf + 34){234+34}})'
+
+const stack = []
+
+const isOpeningBracket = (chr) => ['(', '{'].indexOf(chr) >= 0
+const isClosingBracket = (chr) => [')', '}'].indexOf(chr) >= 0
+
+function checkBrackets (bracketsStr) {
+  for (let i = 0; i < someStr.length; i++) {
+    if (isOpeningBracket(someStr[i])) {
+      stack.push(someStr[i]);
+    }
+    if (isClosingBracket(someStr[i])) {
+      const lastBracket = stack.pop();
+      switch (someStr[i]) {
+        case ')': if (lastBracket !== '(') return false; break;
+        case '}': if (lastBracket !== '{') return false; break;
+      }
+    }
+  }
+  return stack.length === 0
+}
+
+console.log(checkBrackets(someStr))
